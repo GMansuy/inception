af49cef75b8ef046fed9ac5968dec36bf16820b2
gmansuy <gmansuy@gmansuy.gmansuy>
2023-02-07 23:34:40 +0000

    mariadb script

--- srcs/requirements/mariadb/conf/setup_mariadb.sh
+++ srcs/requirements/mariadb/conf/setup_mariadb.sh
@@ -1,6 +1,6 @@
 service mysql start
-mysql -u root
-CREATE DATABASE wordpress;
-CREATE USER 'gmansuy'@'localhost' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'gmansuy'@'localhost';
-FLUSH PRIVILEGES;
+mysql -e "CREATE DATABASE wordpress;"
+mysql -e "CREATE USER 'gmansuy'@'localhost' IDENTIFIED BY '123';"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'gmansuy'@'localhost';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
+mysql -u root --password=123 -e "FLUSH PRIVILEGES;"
