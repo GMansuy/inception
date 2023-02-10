e39eece450469d4ab6e6368977a47468d63d6314
GMansuy <gabriel.mansuy@gmail.com>
2023-02-10 11:34:32 +0100

    new config, might work

--- srcs/requirements/mariadb/conf/setup_mariadb.sh
+++
@@ -1,6 +0,0 @@
-service mysql start
-mysql -e "CREATE DATABASE wordpress;"
-mysql -e "CREATE USER 'gmansuy'@'localhost' IDENTIFIED BY '123';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'gmansuy'@'localhost';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
-mysql -u root --password=123 -e "FLUSH PRIVILEGES;"
